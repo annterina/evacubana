f80559831b424e4cfdb4a58b946936d7b954a06f
Ania Lesniak <anna.julia.lesniak@gmail.com>
2024-01-27 00:12:42 +0100

    Have basic environment support for dev and prod

--- app/configuration/db.py
+++ app/configuration/db.py
@@ -1,8 +1,8 @@
 import os
-import boto3
 import pathlib
-from dotenv import load_dotenv
+import boto3
 from boto3.resources.base import ServiceResource
+from dotenv import load_dotenv
 
 base_dir = pathlib.Path(__file__).parent.parent.parent
 
@@ -10,14 +10,18 @@ load_dotenv(base_dir.joinpath('.env'))
 
 
 class Config:
+    DEV = os.getenv('DEV', False)
     DB_REGION_NAME = os.getenv('DB_REGION_NAME')
     DB_ACCESS_KEY_ID = os.getenv('DB_ACCESS_KEY_ID')
     DB_SECRET_ACCESS_KEY = os.getenv('DB_SECRET_ACCESS_KEY')
 
 
 def initialize_db() -> ServiceResource:
-    dynamodb = boto3.resource('dynamodb',
-                              region_name=Config.DB_REGION_NAME,
-                              aws_access_key_id=Config.DB_ACCESS_KEY_ID,
-                              aws_secret_access_key=Config.DB_SECRET_ACCESS_KEY)
+    if Config.DEV:
+        dynamodb = boto3.resource('dynamodb',
+                                  region_name=Config.DB_REGION_NAME,
+                                  aws_access_key_id=Config.DB_ACCESS_KEY_ID,
+                                  aws_secret_access_key=Config.DB_SECRET_ACCESS_KEY)
+    else:
+        dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
     return dynamodb
